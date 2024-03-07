8e59397b42d55951f9a3a204b9f6392de120f37f
Ali <Alipro.ig7@gmail.com>
2024-03-07 14:38:00 +0300

    Search from wikipedia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'wsearch2';
+  pages = [];
+  constructor(private wikipedia: WikipediaService) {}
+
+  public onTerm(term: string) {
+    this.wikipedia.search(term).subscribe((res: any) => {
+      this.pages = res.query.search;
+      console.log(this.pages);
+      
+    });
+  }
 }
